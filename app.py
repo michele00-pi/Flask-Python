8ea2e251233447326c3e65eb929a14cc3b8f015f
Gabriel Di Michele <leninha.gabriel0@gmail.com>
2026-04-19 15:00:56 -0300

    Estrutura inicial

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print("SALVE SALVE")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def main():
+    return render_template("main.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
