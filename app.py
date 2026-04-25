487144575f7a34a0cf98ecae58db129c4e971f5f
Gabriel Di Michele <leninha.gabriel0@gmail.com>
2026-04-25 10:04:59 -0300

    Resoluçao de bugs e utilização das funçoes do módulo dbFunctions.py

--- app.py
+++ app.py
@@ -7,87 +7,97 @@ db.criar_tabela(conn=conn, cursor=cursor)
 
 app = Flask(__name__)
 
-lista_brasil = []
-lista_gringo = []
-lista_classico = []
+dict_gringo = {"Nome": [], "Tempo": []}
+dict_classico = {"Nome": [], "Tempo": []}
 
 @app.route("/") #Pagina inicial
 
 def main():
+    conn, cursor = db.conectar()
+    db.criar_tabela(conn=conn, cursor=cursor)
     return render_template("main.html")
 
 @app.route("/brasileira", methods=["POST", "GET"]) #Rota das musicas brasileiras
 
 def page_brasileira():
+    conn, cursor = db.conectar() #Tem que deixar dentro da rota, se deixar global quebra
     tipo = "Brasileira"
+    dados = db.buscar_dados(cursor=cursor, tipo=tipo)
     if request.method == 'POST':
 
         Btn = request.form.get("Btn")
 
         if Btn == "limpar":
-            limpar(lista_brasil)
             db.limpar_tabela(conn=conn, cursor=cursor)
+            dados=[()]
 
         elif Btn == "listar":
             nome = request.form.get("BRASILINPUT")
             tempo = request.form.get("TEMPOINPUT")
-            cadastrar(nome, tempo, lista_brasil)
             db.inserir_dados(conn=conn, cursor=cursor, nome=nome, tempo=tempo, tipo=tipo) #Insere  na tabela "Musicas" 
-            dados = db.buscar_dados(cursor=cursor, tipo=tipo)
+            dados = db.buscar_dados(cursor=cursor,tipo=tipo)
         
-    return render_template("brasileira.html", lista_brasil=lista_brasil)
+        conn.close()
+    return render_template("brasileira.html", dados=dados)
 
 
 @app.route("/estrangeira", methods=["POST", "GET"]) #Rota das musicas estrangeiras
 
 def page_estrangeira():
+    conn, cursor = db.conectar()
     tipo = "Estrangeira"
+    dados = db.buscar_dados(cursor=cursor, tipo=tipo)
     if request.method == 'POST':
         Btn = request.form.get("Btn")
 
         if Btn == "limpar":
-            limpar(lista_gringo)
+            limpar(dict_gringo)
             db.limpar_tabela(conn=conn, cursor=cursor)
+            dados=[()]
 
         elif Btn == "listar":
             nome = request.form.get("ESTRANGEIROINPUT")
             tempo = request.form.get("TEMPOINPUT")
-            cadastrar(nome, tempo, lista_gringo)
+            cadastrar(nome, tempo, dict=dict_gringo)
             db.inserir_dados(conn=conn, cursor=cursor, nome=nome, tempo=tempo, tipo=tipo)
+            dados = db.buscar_dados(cursor=cursor, tipo=tipo)
         
-    return render_template("estrangeira.html", lista_gringo=lista_gringo)
+        conn.close()
+    return render_template("estrangeira.html", dict_gringo=dict_gringo)
 
 @app.route("/classica", methods=["POST", "GET"]) #Rota das musicas classicas
 
 def page_classica():
+    conn, cursor = db.conectar()
     tipo = "Classica"
+    dados = db.buscar_dados(cursor=cursor, tipo=tipo)
     if request.method == 'POST':
         Btn = request.form.get("Btn")
 
         if Btn == "limpar":
-            limpar(lista_classico)
+            limpar(dict_classico)
             db.limpar_tabela(conn=conn, cursor=cursor)
+            dados = [()]
     
         elif Btn == "listar":
             nome = request.form.get("CLASSICOINPUT")
             tempo = request.form.get("TEMPOINPUT")
-            cadastrar(nome, tempo, lista=lista_classico)
+            cadastrar(nome, tempo, dict=dict_classico)
             db.inserir_dados(conn=conn, cursor=cursor, nome=nome, tempo=tempo, tipo=tipo)
-           
-    return render_template("classica.html", lista_classico=lista_classico)
+            dados = db.buscar_dados(cursor=cursor, tipo=tipo)
+            
+        conn.close()
+    return render_template("classica.html", dict_classico=dict_classico)
 
-def cadastrar(nome,tempo, lista):
+def cadastrar(nome,tempo, dict):
 
-    lista.append(
-        {
-            "Nome":nome,
-            "Tempo": tempo
-        }
-    )
+    dict["Nome"].append(nome)
+    dict["Tempo"].append(tempo)
 
-def limpar(lista):
+def limpar(dict):
 
-    lista.clear()
+    dict["Nome"].clear()
+    dict["Tempo"].clear()
 
 if __name__ == "__main__":
     app.run(debug=True)
