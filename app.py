2f7cfce51f582812bed93ad452614dd1045cd0e0
Gabriel Di Michele <leninha.gabriel0@gmail.com>
2026-04-29 11:35:49 -0300

    Rotas conectadas com o db

--- app.py
+++ app.py
@@ -2,19 +2,11 @@ from flask import Flask, render_template, request
 import sqlite3
 import database.dbFunctions as db
 
-conn, cursor = db.conectar()
-db.criar_tabela(conn=conn, cursor=cursor)
-
 app = Flask(__name__)
-
-dict_gringo = {"Nome": [], "Tempo": []}
-dict_classico = {"Nome": [], "Tempo": []}
 
 @app.route("/") #Pagina inicial
 
 def main():
-    conn, cursor = db.conectar()
-    db.criar_tabela(conn=conn, cursor=cursor)
     return render_template("main.html")
 
 @app.route("/brasileira", methods=["POST", "GET"]) #Rota das musicas brasileiras
@@ -29,12 +21,14 @@ def page_brasileira():
 
         if Btn == "limpar":
             db.limpar_tabela(conn=conn, cursor=cursor)
+            conn.commit()
             dados=[()]
 
         elif Btn == "listar":
             nome = request.form.get("BRASILINPUT")
             tempo = request.form.get("TEMPOINPUT")
             db.inserir_dados(conn=conn, cursor=cursor, nome=nome, tempo=tempo, tipo=tipo) #Insere  na tabela "Musicas" 
+            conn.commit()
             dados = db.buscar_dados(cursor=cursor,tipo=tipo)
         
         conn.close()
@@ -47,23 +41,24 @@ def page_estrangeira():
     conn, cursor = db.conectar()
     tipo = "Estrangeira"
     dados = db.buscar_dados(cursor=cursor, tipo=tipo)
+
     if request.method == 'POST':
         Btn = request.form.get("Btn")
 
         if Btn == "limpar":
-            limpar(dict_gringo)
             db.limpar_tabela(conn=conn, cursor=cursor)
+            conn.commit()
             dados=[()]
 
         elif Btn == "listar":
             nome = request.form.get("ESTRANGEIROINPUT")
             tempo = request.form.get("TEMPOINPUT")
-            cadastrar(nome, tempo, dict=dict_gringo)
             db.inserir_dados(conn=conn, cursor=cursor, nome=nome, tempo=tempo, tipo=tipo)
+            conn.commit()
             dados = db.buscar_dados(cursor=cursor, tipo=tipo)
         
         conn.close()
-    return render_template("estrangeira.html", dict_gringo=dict_gringo)
+    return render_template("estrangeira.html", dados=dados)
 
 @app.route("/classica", methods=["POST", "GET"]) #Rota das musicas classicas
 
@@ -75,29 +70,20 @@ def page_classica():
         Btn = request.form.get("Btn")
 
         if Btn == "limpar":
-            limpar(dict_classico)
             db.limpar_tabela(conn=conn, cursor=cursor)
+            conn.commit()
             dados = [()]
     
         elif Btn == "listar":
             nome = request.form.get("CLASSICOINPUT")
             tempo = request.form.get("TEMPOINPUT")
-            cadastrar(nome, tempo, dict=dict_classico)
             db.inserir_dados(conn=conn, cursor=cursor, nome=nome, tempo=tempo, tipo=tipo)
+            conn.commit()
             dados = db.buscar_dados(cursor=cursor, tipo=tipo)
             
         conn.close()
-    return render_template("classica.html", dict_classico=dict_classico)
+    return render_template("classica.html", dados=dados)
 
-def cadastrar(nome,tempo, dict):
-
-    dict["Nome"].append(nome)
-    dict["Tempo"].append(tempo)
-
-def limpar(dict):
-
-    dict["Nome"].clear()
-    dict["Tempo"].clear()
 
 if __name__ == "__main__":
     app.run(debug=True)
